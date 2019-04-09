ab228877c76ea6494e0bb06fd6cf112c7f45e676
momenul <bdmomenul@gmail.com>
2019-04-10 01:36:29 +0600

    Dashboard_Add

---
+++ BusinessManagementSystem/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BusinessManagementSystem.Controllers
+{
+    public class ProductsController : Controller
+    {
+        //
+        // GET: /Products/
+        public ActionResult Add()
+        {
+            return View();
+        }
+	}
+}
---
+++ BusinessManagementSystem/Scripts/script.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+
+    $(".dropdown>a").click(function () {
+        if (!$(this).next('.dropdown-content').is(':visible')) {
+            $('.dropdown .dropdown-content').slideUp();
+            $(this).next('.dropdown-content').slideDown();
+        } else {
+            $(this).next('.dropdown-content').slideUp();
+        }
+    });
+
+    $("#ToggleSideNav").on('click', function () {
+        $("div.left-sidebar").toggleClass('activeMiniSideNav');
+        $("div.left-sidebar").toggleClass('lelt-scroll');
+        $(".main-container>.content-box").toggleClass('activeContert-box');
+    });
+
+});
